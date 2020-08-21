23bcadb5d44f262ca214026aa64800997bdf8970
tech-introspectors <teamintrospectors@gmail.com>
2020-08-21 20:48:30 +0530

    Updated js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
-// Creating an array to get present student roll numbers
-var numArr = [];
+// var numArr = [];
 
 function pushData() {
     // get value from the input text
@@ -20,17 +19,22 @@ function pushData() {
     document.getElementById('pText').innerHTML = pval;
     console.log(numArr);
 }
-
-function findMissingNumbers() {
-
-
-    var N = Array.from(Array(Math.max.apply(Math, numArr)).keys()); //Generate number array using the largest int from X
-
-    Array.prototype.diff = function (a) {
-        return this.filter(function (i) { return a.indexOf(i) < 0; }); //Return the difference
-    };
-
-    document.getElementById("missingrollnum").innerHTML =  (N.diff(numArr));
-
+function findMissingNumbers(arr) {
+    // Create sparse array with a 1 at each index equal to a value in the input.
+    var sparse = arr.reduce((sparse, i) => (sparse[i] = 1, sparse), []);
+    // Create array 0..highest number, and retain only those values for which
+    // the sparse array has nothing at that index (and eliminate the 0 value).
+    return [...sparse.keys()].filter(i => i && !sparse[i]);
 }
 
+// Creating an array to get present student roll numbers
+var numArr = [];
+
+// callng this when button clicked
+function result() {
+
+    var result = findMissingNumbers(numArr);
+
+    document.getElementById("missingrollnum").innerHTML = result;
+
+}
