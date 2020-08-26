87eb40bf56a4bf1af1b97cacd8a532934d85c5ca
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-08-26 17:03:00 +0530

    Added function to starting and last roll buttons

--- js/main.js
+++ js/main.js
@@ -1,30 +1,59 @@
+
+function startingRollNo() {
+
+    var startingRollNo = document.getElementById('startingRollNo').value;
+
+    if (startingRollNo == "") {
+        alert("You need to enter 'Starting Roll No' of a student");
+    } else {
+        numArr.push(startingRollNo);
+        console.log(numArr);
+    }
+
+}
+
+function lastRollNo() {
+    var lastRollNo = document.getElementById('lastRollNo').value;
+
+    if (lastRollNo == "") {
+        alert("You need to enter 'Last Roll No' of a student");
+    } else {
+        var lastNum = Number(lastRollNo);
+        numArr.push(lastNum + 1);
+        console.log(numArr);
+    }
+}
+
+
+
+
 // This function is used to push the data from the textbox to an array
 function pushData() {
     // get value from the input text
     var inputText = document.getElementById('inputText').value;
 
-    if(inputText == ""){
-       alert("You must enter present students roll no to continue ...!");
-    }else{
+    if (inputText == "") {
+        alert("You must enter present students roll no to continue ...!");
+    } else {
         // append data to the array
-    numArr.push(inputText);
+        numArr.push(inputText);
 
-    var pval = "";
+        var pval = "";
 
-    for (i = 0; i < numArr.length; i++) {
-        pval = pval + numArr[i] + ", ";
+        for (i = 0; i < numArr.length; i++) {
+            pval = pval + numArr[i] + ", ";
+        }
+
+        // display array data in pText box
+        document.getElementById('pText').innerHTML = "<b>" + pval;
+
+        // remove value from textbox after data is pushed in an array
+        document.getElementById("inputText").value = "";
+
+        console.log(numArr);
     }
 
-    // display array data in pText box
-    document.getElementById('pText').innerHTML = "<b>" + pval;
 
-    // remove value from textbox after data is pushed in an array
-    document.getElementById("inputText").value = "";
-
-    console.log(numArr);
-    }
-    
-    
 }
 
 
@@ -40,33 +69,49 @@ var numArr = [];
 function result() {
 
     var result = missingNumbers(numArr);
-    document.getElementById("missingrollnum").innerHTML = "<b>" + result ;
+    document.getElementById("missingrollnum").innerHTML = "<b>" + result;
 
 }
 
 // Function to reset 
-function reset(){
+function reset() {
     // remove array elements
-     while(numArr.length > 0){
-         numArr.pop();
-     }
+    while (numArr.length > 0) {
+        numArr.pop();
+    }
 
-     // removing textbox content
-     document.getElementById("inputText").value = "";
+    // removing startingRollNo value from array
+    document.getElementById("startingRollNo").innerHTML = "";
 
-     // removing present student roll no content
-     document.getElementById("pText").innerHTML = "";
+    // removing startingRollNo textbox content
+    document.getElementById("startingRollNo").value = "";
 
-     // removing absent students roll no content
-     document.getElementById("missingrollnum").innerHTML = "";
 
-     console.log(numArr);
+
+    // removing lastRollNo value from array
+    document.getElementById("lastRollNo").innerHTML = "";
+
+    // removing lastRollNo textbox content
+    document.getElementById("lastRollNo").value = "";
+
+
+
+    // removing textbox content
+    document.getElementById("inputText").value = "";
+
+    // removing present student roll no content
+    document.getElementById("pText").innerHTML = "";
+
+    // removing absent students roll no content
+    document.getElementById("missingrollnum").innerHTML = "";
+
+    console.log(numArr);
 }
 
 
 // On presss enter key add elements into an array
-document.getElementById("addbtn").addEventListener("keyup", function(event) {
+document.getElementById("addbtn").addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
-		return true;
+        return true;
     }
 });
